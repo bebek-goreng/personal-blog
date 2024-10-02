a5a4d40a53461c5b5581670e8f3199f5dbba2d82
mahen <oresuki01@gmail.com>
2024-10-02 13:24:26 +0700

    feat: user services

---
+++ server/src/services/user-service.js
@@ -0,0 +1,42 @@
+import { UserRepositories } from "../repositories/user-repository.js";
+import { Jwt } from "../utils/jwt.js";
+
+export class UserController {
+    static async login(data) {
+        const user = await UserRepositories.login(data.email);
+
+        if (!user) {
+            throw { status: 401, message: "Invalid credentials" }
+        }
+
+        const comparePassword = await Bcrypt.comparePassword(data.password, user.password)
+
+        if (!comparePassword) {
+            throw { status: 401, message: "Invalid credentials" }
+        }
+
+        const token = Jwt.generateToken({
+            id: user.id,
+            username: user.username,
+            email: user.email
+        });
+
+        return token;
+    }
+
+    static async register(data) {
+        const existingUser = await UserRepositories.login(data.email);
+
+        if (existingUser) {
+            throw { status: 400, message: "Email already taken" }
+        }
+
+        const newUser = await UserRepositories.register(data);
+
+        if (!newUser) {
+            throw { status: 400, message: "register failed, internal server error" }
+        }
+
+        return newUser;
+    }
+}
